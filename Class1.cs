b2f2ec392f0cc029972018112be85eab079b3a23
Nara19941008 <narminim@code.edu.az>
2022-06-23 01:46:59 +0400

    Added bio table

---
+++ Class1.cs
@@ -0,0 +1,216 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Array_OOP_Class
+//{
+//    class Class1
+//    {
+//        static void Main(string[] args)
+//        {
+            //    int[] numbers = new int[6];
+            //    numbers[0] = 3;
+            //    numbers[1] = 5;
+            //    numbers[2] = 100;
+            //    numbers[10] = 66;
+
+
+            //    Array.Resize(ref numbers, 20);
+
+            //int[] nums = { 1, 2, 5, 6, 7, 55, 25 };
+            //Array.Sort(nums);
+
+            //foreach (var item in nums)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            //int[,] arr =
+            //{
+            //    {1,2},
+            //    {3,4},
+            //    {5,6}
+            //};
+            //Console.WriteLine(nums.Rank);
+
+            //foreach (var item in numbers)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+
+
+
+
+
+
+            //string name1 = "Orxan";
+            //string surname1 = "Akbarov";
+            //int age1 = 26;
+
+
+            //string name2 = "Emil";
+            //string surname2 = "Abdullayev";
+            //int age2 = 20;
+
+
+            //string name3 = "Fidan";
+            //string surname3 = "Merdanli";
+            //int age3 = 17;
+
+
+            //Console.WriteLine(name1 + " " + surname1);
+
+
+
+            //var obj1 = new
+            //{
+            //    name = "Orxan",
+            //    surname = "Akbarov",
+            //    age = 26
+            //};
+
+
+            //var obj2 = new
+            //{
+            //    name = "Emil",
+            //    surname = "Abdullayev",
+            //    age = 20
+            //};
+
+
+            //Console.WriteLine($"{obj2.name} {obj2.surname} {obj2.age}");
+
+
+
+            //Student st1 = new Student("Ramil",30);
+            //st1.name = "Orxan";
+            //st1.surname = "Akbarov";
+            //st1.age = 26;
+
+            //Student st2 = new Student();
+            //st2.name = "Emil";
+            //st2.surname = "Abdullayev";
+            //st2.age = 20;
+
+            //Console.WriteLine(st1.FullName());
+
+            //Student st3 = new Student
+            //{
+            //    name = "Cavid",
+            //    surname = "Basirov",
+            //    age = 28
+            //};
+
+
+            //    Student student = new Student();
+            //    student.name = "Orxan";
+
+            //    var result = student.GetGroupInfo();
+
+
+            //    Console.WriteLine(result);
+
+
+
+
+            //}
+
+
+
+
+        }
+
+
+        //class Student
+        //{
+        //    public string name;
+        //    public string surname;
+        //    public int age;
+        //    public string address;
+
+
+        //    public Student()
+        //{
+        //    Console.WriteLine("Empty");
+        //}
+
+
+        //public Student(int age, string addresssss) : this()
+        //{
+        //    this.address = addresssss;
+        //    Console.WriteLine("Age" + age);
+        //}
+
+
+
+
+
+        //public Student(int stuName)
+        //{
+        //    Console.WriteLine("Name:" + stuName);
+        //}
+
+
+
+
+        //public Student(string studentName, int studentAge)
+        //{
+        //    name = studentName;
+        //    age = studentAge;
+        //    Console.WriteLine(name + " " + age);
+        //}
+
+
+
+
+
+
+
+
+
+
+        //public Student()
+        //{
+        //    Console.WriteLine("constructor");
+        //}
+
+
+
+
+
+
+        //public string FullName()
+        //{
+        //    return name + " " + surname;
+
+
+        //}
+
+        //public string GetGroupInfo()
+        //    {
+        //        Teacher teacher = new Teacher();
+        //        teacher.name = "Cavid";
+
+        //        return $"Teacher name:{teacher.GetTeacherName()} Student name:{name}";
+        //    }
+        //}
+
+
+
+        //class Teacher
+        //{
+        //    public string name;
+        //    public decimal salary;
+
+        //    public string GetTeacherName()
+        //    {
+        //        return name;
+        //    }
+        //}
+
+
+//    }
+//}
